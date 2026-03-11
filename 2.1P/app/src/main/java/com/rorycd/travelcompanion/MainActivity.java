c5380d530aba181c46076589d013c908fb5d82b9
rory-cd <rory.doug@gmail.com>
2026-03-11 12:47:58 +1100

    Fix number formatting and refactor

--- 2.1P/app/src/main/java/com/rorycd/travelcompanion/MainActivity.java
+++ 2.1P/app/src/main/java/com/rorycd/travelcompanion/MainActivity.java
@@ -3,6 +3,7 @@ package com.rorycd.travelcompanion;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -15,42 +16,76 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
 import java.util.Currency;
 import java.util.HashMap;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etInput;
+    EditText etInput, etOutput;
+    Currency inputCurrency, outputCurrency;
+    Spinner spinInputCurrency, spinOutputCurrency;
+    TextView tvInputPrefix, tvOutputPrefix;
 
-    Currency currencyFrom;
-    Spinner spinFromCurrency;
-    Currency currencyTo;
-    Spinner spinToCurrency;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-    TextView tvFromPrefix;
-    TextView tvToPrefix;
-    EditText etOutput;
+        // Get UI Elements
+        etInput = findViewById(R.id.etInput);
+        etOutput = findViewById(R.id.etOutput);
+        spinInputCurrency = findViewById(R.id.spinOutputCurrency);
+        spinOutputCurrency = findViewById(R.id.spinInputCurrency);
+        tvInputPrefix = findViewById(R.id.tvInputPrefix);
+        tvOutputPrefix = findViewById(R.id.tvOutputPrefix);
+
+        // Set initial currencies and hints
+        inputCurrency = Currency.getInstance(spinInputCurrency.getSelectedItem().toString());
+        outputCurrency = Currency.getInstance(spinOutputCurrency.getSelectedItem().toString());
+        etInput.setHint(String.format(Locale.getDefault(), "%.2f", 0.00));
+        etOutput.setHint(String.format(Locale.getDefault(), "%.2f", 0.00));
+
+        // Set listeners
+        etInput.addTextChangedListener(inputWatcher);
+        etOutput.addTextChangedListener(outputWatcher);
+        etInput.setOnFocusChangeListener(new EditTextFocusChangeListener(etInput, inputWatcher));
+        etOutput.setOnFocusChangeListener(new EditTextFocusChangeListener(etOutput, outputWatcher));
+        spinInputCurrency.setOnItemSelectedListener(new OnSpinnerSelectedListener(tvInputPrefix, c -> inputCurrency = c));
+        spinOutputCurrency.setOnItemSelectedListener(new OnSpinnerSelectedListener(tvOutputPrefix, c -> outputCurrency = c));
+    }
+
+    protected interface CurrencySelector { void selectCurrency(Currency currency); }
 
     private class OnSpinnerSelectedListener implements AdapterView.OnItemSelectedListener {
 
         private TextView prefix;
+        CurrencySelector selector;
 
-        OnSpinnerSelectedListener(TextView prefix) {
+        OnSpinnerSelectedListener(TextView prefix, CurrencySelector selector) {
             this.prefix = prefix;
+            this.selector = selector;
         }
 
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
             // Set currency
             String code = parent.getSelectedItem().toString();
-            currencyFrom = Currency.getInstance(code);
+            selector.selectCurrency(Currency.getInstance(code));
 
-            // Apply correct symbol
-            String symbol = currencyFrom.getSymbol(Locale.US);  // US uses symbol only
-            if (symbol.contains("$")) symbol = "$";             // Ignore "A$" etc.
+            // Apply correct symbol prefix
+            String symbol = inputCurrency.getSymbol(Locale.US);  // US uses symbol only
+            if (symbol.contains("$")) symbol = "$";              // Ignore "A$" etc.
             prefix.setText(symbol);
 
+            // Update output
             etOutput.removeTextChangedListener(outputWatcher);
             applyConversion(etInput, etOutput);
             etOutput.addTextChangedListener(outputWatcher);
@@ -86,41 +121,60 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    // Clear leading and trailing zeroes from input fields
+    // Clean up input fields on unfocus
     private class EditTextFocusChangeListener implements View.OnFocusChangeListener {
 
         private EditText editText;
+        private TextWatcher watcher;
 
-        EditTextFocusChangeListener(EditText editText) {
+        EditTextFocusChangeListener(EditText editText, TextWatcher watcher) {
             this.editText = editText;
+            this.watcher = watcher;
         }
 
         @Override
         public void onFocusChange(View v, boolean hasFocus) {
             if (!hasFocus) {
                 String s = editText.getText().toString();
-                editText.removeTextChangedListener(inputWatcher);
-                editText.setText(s.replaceAll("(^0+)|(\\.0*$)|(0+$)", ""));
-                editText.addTextChangedListener(inputWatcher);
+                if (s.isEmpty()) return;
+
+                editText.removeTextChangedListener(watcher);
+
+                // Clean up input
+                if (s.equals(".")) editText.setText("");
+                else {
+                    double newInput = Double.parseDouble(s);
+                    editText.setText(formatNumber(newInput, 0, 50));
+                }
+
+                editText.addTextChangedListener(watcher);
             }
         }
     }
 
     protected void applyConversion(EditText origin, EditText target) {
         String strInput = origin.getText().toString();
-        if (strInput.isEmpty()) strInput = "0.00";
+        if (strInput.isEmpty() || strInput.equals(".")) strInput = "0";
         double newInput = Double.parseDouble(strInput);
-        double convertedInput = convertCurrency(newInput, currencyFrom, currencyTo);
-        if (convertedInput > 0) {
-            target.setText(String.format(Locale.getDefault(), "%.2f", convertedInput));
-        } else {
-            target.setText("");
-        }
+        double convertedInput = convertCurrency(newInput);
+        target.setText(formatNumber(convertedInput, 0, 2));
     }
 
-    protected double convertCurrency(double value, Currency from, Currency to) {
-        String fromCode = from.getCurrencyCode();
-        String toCode = to.getCurrencyCode();
+    protected String formatNumber(double number, int minFractionDigits, int maxFractionDigits) {
+        // Base format on locale
+        DecimalFormat df = (DecimalFormat) DecimalFormat.getInstance(Locale.getDefault());
+        // Set digits after decimal "0.XXX"
+        df.setMinimumFractionDigits(minFractionDigits);
+        df.setMaximumFractionDigits(maxFractionDigits);
+        // Format number as string
+        String formatted = df.format(number);
+        if (formatted.equals("0")) formatted = "";
+        return formatted;
+    }
+
+    protected double convertCurrency(double value) {
+        String inputCode = inputCurrency.getCurrencyCode();
+        String outputCode = outputCurrency.getCurrencyCode();
 
         // Get USD -> target rate
         HashMap<String, Double> rates = new HashMap<>();
@@ -131,44 +185,11 @@ public class MainActivity extends AppCompatActivity {
         rates.put("JPY", 148.50);
 
         // Convert to USD
-        double valueUSD = value / rates.get(fromCode);
+        double valueUSD = value / rates.get(inputCode);
 
         // Convert to target currency
-        return valueUSD * rates.get(toCode);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        // Get UI Elements
-        etInput = findViewById(R.id.etInput);
-        etOutput = findViewById(R.id.etOutput);
-        spinFromCurrency = findViewById(R.id.spinFromCurrency);
-        spinToCurrency = findViewById(R.id.spinToCurrency);
-        tvFromPrefix = findViewById(R.id.tvFromPrefix);
-        tvToPrefix = findViewById(R.id.tvToPrefix);
-
-        // Set initial currencies and hints
-        currencyFrom = Currency.getInstance(spinFromCurrency.getSelectedItem().toString());
-        currencyTo = Currency.getInstance(spinToCurrency.getSelectedItem().toString());
-        etInput.setHint(String.format(Locale.getDefault(), "%.2f", 0.00));
-        etOutput.setHint(String.format(Locale.getDefault(), "%.2f", 0.00));
-
-        // Set listeners
-        etInput.addTextChangedListener(inputWatcher);
-        etOutput.addTextChangedListener(outputWatcher);
-        etInput.setOnFocusChangeListener(new EditTextFocusChangeListener(etInput));
-        etOutput.setOnFocusChangeListener(new EditTextFocusChangeListener(etOutput));
-        spinFromCurrency.setOnItemSelectedListener(new OnSpinnerSelectedListener(tvFromPrefix));
-        spinToCurrency.setOnItemSelectedListener(new OnSpinnerSelectedListener(tvToPrefix));
-
+        Log.d("CONVERT", "USD: " + valueUSD);
+        Log.d("CONVERT", "new: " + valueUSD * rates.get(outputCode));
+        return valueUSD * rates.get(outputCode);
     }
 }
