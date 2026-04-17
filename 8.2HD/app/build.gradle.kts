bd0d45e328c5c8d49b79bc7be2fb669d39a613d7
rory-cd <rory.doug@gmail.com>
2026-04-17 14:12:21 +1000

    Upgrade min API to 26

--- 8.2HD/app/build.gradle.kts
+++ 8.2HD/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rorycd.bowerbird"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -55,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // WorkManager dependency
+    implementation("androidx.work:work-runtime-ktx:2.11.2")
 }
